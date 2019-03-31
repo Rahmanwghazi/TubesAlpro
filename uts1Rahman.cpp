be3b968e3839ca851351f935dc5928145b61a39d
M. Rahman Wafiq Ghazi <49003404+Rahmanwghazi@users.noreply.github.com>
2019-03-31 20:02:38 +0800

    Add files via upload

---
+++ uts1Rahman.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Nama     : M. Rahman Wafiq Ghazi"<<endl;
+    cout<<"NIM      : 1829141003"<<endl;
+    cout<<"Kelas    : Teknik Komputer B"<<endl;
+    cout<<"ANgkatan : 2018"<<endl;
+    return 0;
+
+}
